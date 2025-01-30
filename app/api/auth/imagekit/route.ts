c63f418ba69d318e62fc58209a1beb7f7e95887c
[Ahmed390650] <ahmed_elsayed_9961@yahoo.com>
2025-01-30 22:36:42 +0200

    imageJit

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,17 +1,15 @@
-import config from "@/lib/config";
 import ImageKit from "imagekit";
+import config from "@/lib/config";
 import { NextResponse } from "next/server";
+
 const {
   env: {
-    imageKit: { privateKey, publicKey, urlEndpoint },
+    imageKit: { publicKey, privateKey, urlEndpoint },
   },
 } = config;
-const imageKit = new ImageKit({
-  privateKey,
-  publicKey,
-  urlEndpoint,
-});
+
+const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
 
 export async function GET() {
-  return NextResponse.json(imageKit.getAuthenticationParameters());
+  return NextResponse.json(imagekit.getAuthenticationParameters());
 }
